812e346d56155609cfc3b86849547142c437097d
holloweyed <Galuzavioletta-2005@mail.ru>
2024-11-08 00:20:18 +0300

    commit

---
+++ casd_labs/Class1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casd_labs
+{
+    internal class Class1
+    {
+        public class MyArrayList<T>
+        {
+            private T[] elementData;
+            private int size;
+
+            public MyArrayList()
+            {
+                elementData = new T[0];
+                size = 0;
+            }
+
+            public MyArrayList(T[] a)
+            {
+                elementData = a;
+                size = a.Length;
+            }
+
+            public MyArrayList(int capacity)
+            {
+                elementData = new T[capacity];
+                size = 0;
+            }
+
+            public void Add(T e)
+            {
+                if (size == elementData.Length)
+                {
+                    T[] a = new T[(int)(size * 1.5 + 1)];
+                    for (int i = 0; i < size; i++) a[i] = elementData[i];
+                    elementData = a;
+                }
+
+                elementData[size] = e;
+            }
+
+            public void AddAll(T[] a)
+            {
+                for (int i = 0; i < a.Length; i++) Add(a[i]);
+            }
+
+            public void Clear()
+            {
+                elementData = null;
+                size = 0;
+            }
+
+            public bool Contains(T o)
+            {
+                if (!(elementData == null))
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (elementData[i].Equals(o)) return true;
+                    }
+                }
+                return false;
+            }
+
+            public bool ContainsAll(T[] a)
+            {
+                int f = 1;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (!Contains(a[i])) f = 0;
+                }
+                if (f == 0) return false;
+                return true;
+            }
+
+            public bool IsEmpty()
+            {
+                if (size == 0) return true;
+                else return false;
+            }
+
+            public void Remove(T o)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (elementData[i].Equals(o))
+                    {
+                        for (int j = i; j < size - 1; j++)
+                        {
+                            elementData[j] = elementData[j + 1];
+                        }
+                        size--;
+                    }
+                }
+            }
+
+            public void RemoveAll(T[] a)
+            {
+                for (int i = 0; i < a.Length; i++) Remove(a[i]);
+            }
+
+            public void RetainAll(T[] a)
+            {
+                for (int i = 0; i < a.Length; i++) elementData[i] = a[i];
+                size = a.Length;
+            }
+
+            public int Size()
+            {
+                return size;
+            }
+
+            public T[] ToArray()
+            {
+                T[] a = new T[size];
+                for (int i = 0; i < size; i++) a[i] = elementData[i];
+                return a;
+            }
+
+            public void ToArray(ref T[] a)
+            {
+                if (a.Length <= size && a.Length > 0)
+                {
+                    T[] arr = new T[a.Length + size];
+                    for (int i = 0; i < a.Length; i++) arr[i] = a[i];
+                    for (int i = a.Length; i < size; i++) arr[i] = elementData[i];
+                    a = arr;
+
+                }
+                else
+                {
+                    T[] arr = new T[size];
+                    for (int i = 0; i < size; i++) arr[i] = elementData[i];
+                    a = arr;
+                }
+            }
+
+            public void Add(int index, T e)
+            {
+                if (size == elementData.Length)
+                {
+                    T[] a = new T[(int)(size * 1.5 + 1)];
+                    for (int i = 0; i < size; i++) a[i] = elementData[i];
+                    elementData = a;
+                }
+                for (int i = size; i > index; i--)
+                {
+                    elementData[i] = elementData[i - 1];
+                }
+
+                elementData[index] = e;
+                size++;
+            }
+
+            public void AddAll(int index, T[] a)
+            {
+                foreach (T e in a) Add(index, e);
+            }
+
+            public T Get(int index)
+            {
+                return elementData[index];
+            }
+
+            public int IndexOf(T o)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (elementData[i].Equals(o)) return i;
+                }
+                return -1;
+            }
+
+            public int LastIndexOf(T o)
+            {
+                for (int i = size - 1; i > 0; i--)
+                {
+                    if (elementData[i].Equals(o)) return i;
+                }
+                return -1;
+            }
+
+            public T Remove(int index)
+            {
+                T remElem = elementData[index];
+                for (int i = index; i < size - 1; i++)
+                {
+                    elementData[i] = elementData[i + 1];
+                }
+                size--;
+                return remElem;
+            }
+
+            public void Set(int index, T e)
+            {
+                elementData[index] = e;
+            }
+
+            public MyArrayList<T> SubList(int fromindex, int toindex)
+            {
+                T[] sArr = new T[toindex - fromindex];
+                for (int i = fromindex + 1; i < sArr.Length; i++)
+                {
+                    sArr[i] = elementData[i];
+                }
+                MyArrayList<T> sub = new MyArrayList<T>(sArr);
+                return sub;
+            }
+        }
+    }
+}
--- casd_labs/Program.cs
+++ casd_labs/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static casd_labs.Class1;
+using static System.Net.WebRequestMethods;
 
 namespace casd_labs
 {
@@ -10,6 +13,57 @@ namespace casd_labs
     {
         static void Main(string[] args)
         {
+            MyArrayList<string> tags = new MyArrayList<string>();
+            try
+            {
+                using (StreamReader reader = new StreamReader("C:\\Users\\galuz\\Desktop\\file1.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        int startInd = line.IndexOf('<');
+                        while (startInd != -1)
+                        {
+                            int endInd = line.IndexOf('>', startInd);
+
+                            string tag = line.Substring(startInd + 1, endInd - startInd - 1);
+
+                            if (tag.Length >= 2 && (char.IsLetter(tag[1]) || (tag[1] == '/' && char.IsLetter(tag[2]))))
+                            {
+                                bool f = true;
+                                for (int i = 2; i < tag.Length; i++)
+                                {
+                                    if (!char.IsLetterOrDigit(tag[i]))
+                                    {
+                                        f = false;
+                                        break;
+                                    }
+                                }
+                                string tag2;
+                                if (tag[0] == '/') tag2 = tag.Substring(1, tag.Length - 1);
+                                else tag2 = tag;
+
+                                if (!tags.Contains(tag2.ToUpper()) && f == true)
+                                {
+                                    tags.Add(tag2.ToUpper());
+                                }
+
+                            }
+
+                            startInd = line.IndexOf('<', endInd + 1);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception : " + e.Message);
+            }
+            finally
+            {
+                for (int i = 0; i < tags.Size(); i++) Console.WriteLine(tags.Get(i).ToString());
+            }
+            Console.ReadLine();
         }
     }
 }
