4dc0e84ea3079fff7088b28e4b6ad54e3dea7c6d
holloweyed <Galuzavioletta-2005@mail.ru>
2024-11-09 09:13:14 +0300

    commit

---
+++ casd_labs/Class2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Request : IComparable<Request>
+    {
+        private int priority;
+        private int number;
+        private int step;
+        private Stopwatch stopwatch;
+
+        public int Priority => priority;
+        public int Number => number;
+        public int Step => step;
+        public Stopwatch Stopwatch => stopwatch;
+
+        public Request(int priority, int number, int step)
+        {
+            this.priority = priority;
+            this.number = number;
+            this.step = step;
+            stopwatch = Stopwatch.StartNew();
+        }
+
+        ~Request()
+        {
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+        }
+
+        public int CompareTo(Request other)
+        {
+            if (other == null)
+                return 1;
+            return this.priority.CompareTo(other.priority);
+        }
+    }
+}
--- casd_labs/Program.cs
+++ casd_labs/Program.cs
@@ -1,16 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace casd_labs
+namespace ConsoleApp1
 {
     internal class Program
     {
+        private static int numberRequest = 1;
+        private static string filePath = "log.txt";
+        private static StreamWriter writer = new StreamWriter(filePath);
+
         static void Main(string[] args)
         {
+            try
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                MyPriorityQueue<Request> queue = new MyPriorityQueue<Request>();
+                for (int i = 1; i < n + 1; i++)
+                {
+                    Random rnd = new Random();
+                    int countRequests = rnd.Next(1, 10);
+                    for (int j = 0; j < countRequests; j++)
+                    {
+                        GenerateRequest(queue, i, countRequests);
+                    }
+                    Request maxRequest = queue.Element();
+                    queue.Remove(maxRequest);
+                    writer.WriteLine("REMOVE " + maxRequest.Number + " " + maxRequest.Priority + " " + maxRequest.Step);
+                }
+                System.TimeSpan time = new TimeSpan();
+                while (!queue.IsEmpty())
+                {
+                    Request req = queue.Element();
+                    time = req.Stopwatch.Elapsed;
+                    queue.Remove(req);
+                    writer.WriteLine("REMOVE " + req.Number + " " + req.Priority + " " + req.Step);
+                }
+                queue = null;
+                Console.WriteLine(time);
+                writer.Close();
+                Console.WriteLine("Данные записаны в файл: " + Directory.GetCurrentDirectory() + "\\log.txt");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }
+        }
+        private static void GenerateRequest(MyPriorityQueue<Request> queue, int step, int countRequests)
+        {
 
+            Random rnd = new Random();
+            
+            for (int i = 0; i <= countRequests; i++)
+            {
+                int priority = rnd.Next(1, 5);
+                Request request = new Request(priority, numberRequest, step);
+                numberRequest++;
+                queue.Add(request);
+                writer.WriteLine("ADD " + request.Number + " " + request.Priority + " " + request.Step);
+            }
         }
     }
 }
